d34be943d9f665f861c1fc3c4aa63e3c6e9c82f5
Luqzan Ariff <luqzanariff@gmail.com>
2024-10-09 13:24:58 +0800

    Actions in Livewire

--- app/Livewire/CreatePoll.php
+++ app/Livewire/CreatePoll.php
@@ -8,8 +8,19 @@ class CreatePoll extends Component
 {
     public $title;
 
+    public $options = [''];
+
     public function render()
     {
         return view('livewire.create-poll');
     }
+
+    public function addOption()
+    {
+        $this->options[] = '';
+    }
+
+    // public function mount() {
+
+    // }
 }
--- resources/views/livewire/create-poll.blade.php
+++ resources/views/livewire/create-poll.blade.php
@@ -3,5 +3,17 @@
 		<label>Poll Title</label>
 		<input type="text" wire:model.live="title" />
 		Current title: {{ $title }}
+
+		<div class="mb-4">
+			<button class="btn mt-4" wire:click.prevent="addOption">Add Option</button>
+		</div>
+
+		<div class="mt-4">
+			@foreach ($options as $index => $option)
+				<div class="mb-4">
+					{{ $index }} - {{ $option }}
+				</div>
+			@endforeach
+		</div>
 	</form>
 </div>
