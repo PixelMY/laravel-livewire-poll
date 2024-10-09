461c0598fcf6e1ae4234bfd7e545f4b9cccd05e2
Luqzan Ariff <luqzanariff@gmail.com>
2024-10-09 15:36:03 +0800

    Voting in Polls

--- app/Livewire/Polls.php
+++ app/Livewire/Polls.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire;
 
+use App\Models\Option;
 use App\Models\Poll;
 use Livewire\Attributes\On;
 use Livewire\Component;
@@ -14,5 +15,10 @@ class Polls extends Component
         $polls = Poll::with('options.votes')->latest()->get();
 
         return view('livewire.polls', ['polls' => $polls]);
+    }
+
+    public function vote(Option $option)
+    {
+        $option->votes()->create();
     }
 }
