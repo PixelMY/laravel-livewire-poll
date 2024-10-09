5ff4393b7cfb36081f0fa25fb5a4a553dd726462
Luqzan Ariff <luqzanariff@gmail.com>
2024-10-09 13:47:17 +0800

    Creating a Poll

--- resources/views/livewire/create-poll.blade.php
+++ resources/views/livewire/create-poll.blade.php
@@ -1,8 +1,7 @@
 <div>
-	<form>
+	<form wire:submit.prevent="createPoll()">
 		<label>Poll Title</label>
 		<input type="text" wire:model.live="title" />
-		Current title: {{ $title }}
 
 		<div class="mb-4">
 			<button class="btn mt-4" wire:click.prevent="addOption()">Add Option</button>
@@ -14,11 +13,13 @@
 					<label>Option {{ $index + 1 }}</label>
 
 					<div class="flex gap-2">
-						<input type="text" wire:model.live="options.{{ $index }}" />
+						<input type="text" wire:model="options.{{ $index }}" />
 						<button class="btn" wire:click.prevent="removeOption({{ $index }})">Remove</button>
 					</div>
 				</div>
 			@endforeach
 		</div>
+
+		<button type="submit" class="btn">Create Poll</button>
 	</form>
 </div>
